2de37fd800983d4468d2e2c9c1f99959e3cc9605
osalmeron2024 <osalmeron2024@hotmail.com>
2024-10-04 17:30:50 -0600

    snapShop-04102024

---
+++ js/rutina.js
@@ -0,0 +1,8 @@
+const boton = document.querySelector("#boton");
+
+const tamañoPantalla = () => {
+    const v_width = window.screen.width;
+    alert(`Èl ancho de la pantalla es: ${v_width}`);
+}
+
+// boton.addEventListener("click",tamañoPantalla);
